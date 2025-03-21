161f251e2cfb30cef73515b1067c497b920cf762
Neeraj Chaudhary <neerajofficial1919@gmail.com>
2025-03-21 15:57:07 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://job-portal-euk2.onrender.com',
   credentials: true,
 };
 app.use(cors(corsOptions));
